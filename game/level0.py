be13c6a64a9447d26d1c980febf5f0be15d1a8f8
Simon Lovec <simon.lovec@email.cz>
2026-04-24 20:59:29 +0200

    Improve the tutorial to have a cleaner solution

--- game/level0.py
+++ game/level0.py
@@ -1,4 +1,4 @@
-from robot_programming_game import Game, RobotAction, FlagColor, TileInfo, Direction
+from robot_programming_game import Game, Robot, RobotAction, FlagColor, TileInfo, Direction
 
 # Level 0 - Tutorial
 
@@ -42,35 +42,36 @@ if coin_position is None:
     raise Exception("Coin is missing!")
 
 # since there are no walls and we can see the coin directly
-# we can just move to the coin straigh away
+# we can just move to the coin straight away
 
 # at the start the robot is facing to the right
 # print(robot.direction)
 
-i = 0
+def get_next_action(robot: Robot, coin_position: tuple[int, int]):
+    dx = coin_position[0] - robot.x
+    dy = coin_position[1] - robot.y
+
+    # get desired direction
+    if dx > 0:
+        target = Direction.Right
+    elif dx < 0:
+        target = Direction.Left
+    elif dy > 0:
+        target = Direction.Down
+    else:
+        target = Direction.Up
+
+    if robot.direction == target:
+        return RobotAction.MoveForward
+    else: # turn until we are at the desired position
+        return RobotAction.TurnLeft
+
 while game.step():
     # you can use flags to mark the tiles we have already visited
     level.set_flag(robot.x, robot.y, FlagColor.Blue)
 
-    # if the coin is to the right from us, move forward (since we are already facing right)
-    if robot.direction == Direction.Right and robot.x < coin_position[0]:
-        robot.set_action(RobotAction.MoveForward)
-    # if the coin is to the left from us, we need to turn, when we are facing down we need to turn again to turn 180deg
-    elif (robot.direction == Direction.Right or robot.direction == Direction.Down) and robot.x > coin_position[0]:
-        robot.set_action(RobotAction.TurnRight)
-    # if the coin is to the left from us and we are facing left, move forward
-    elif robot.direction == Direction.Left and robot.x > coin_position[0]:
-        robot.set_action(RobotAction.MoveForward)
-    # if the coin is directly above or below us 
-    elif (robot.direction == Direction.Right or robot.direction == Direction.Left) and robot.x == coin_position[0]:
-        # turn towards the coin
-        if robot.y < coin_position[1]:
-            robot.set_action(RobotAction.TurnRight if robot.direction == Direction.Right else RobotAction.TurnLeft)
-        elif robot.y > coin_position[1]:
-            robot.set_action(RobotAction.TurnLeft if robot.direction == Direction.Right else RobotAction.TurnRight)
-    # we are facing towards the coin, move forward
-    else:
-        robot.set_action(RobotAction.MoveForward)
+    # decide the next action and give it to the robot
+    robot.set_action(get_next_action(robot, coin_position))
 
 # this one was very simple, since we could see the whole board from the start
 # what if there was a wall that obstructed the robots vision?
